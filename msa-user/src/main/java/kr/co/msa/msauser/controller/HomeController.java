329e702c703cef2a9b92c1567bcae2a2629c65dc
changhyun <hn12344@naver.com>
2021-07-16 10:15:06 +0900

    user-service gatewayapi

--- msa-user/src/main/java/kr/co/msa/msauser/controller/HomeController.java
+++ msa-user/src/main/java/kr/co/msa/msauser/controller/HomeController.java
@@ -1,7 +1,14 @@
 package kr.co.msa.msauser.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user-service")
 public class HomeController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome to user service";
+    }
 }
