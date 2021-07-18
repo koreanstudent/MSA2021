be719c7f9194e8e9cda9b34ac7852fb71bce323d
이창현 <hn12344@naver.com>
2021-07-19 02:16:14 +0900

    config-service 추가

---
+++ msa-config/src/test/java/kr/co/msa/msaconfig/MsaConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.msa.msaconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
