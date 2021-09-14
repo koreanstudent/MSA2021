9cddd69d702c404347cd8943f67d77b90e223357
changhyun <hn12344@naver.com>
2021-09-14 10:19:10 +0900

    catalog service add

---
+++ msa-catalog/src/test/java/kr/co/msa/msacatalog/MsaCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.msa.msacatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
